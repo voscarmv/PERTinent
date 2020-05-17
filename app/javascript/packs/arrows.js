c848f18511749dd38a522711f9fe493f30e527e9
Oscar Mier <voscarmv@gmail.com>
2020-05-17 14:19:28 -0500

    Set SVG height with javascript.

--- app/javascript/packs/arrows.js
+++ app/javascript/packs/arrows.js
@@ -1,4 +1,6 @@
 
+$("#svg_bg").attr("height", $(document).height());
+
 var drawConnector = function(divA, divB, edge) {
   let elemA = $(divA)
   let elemB = $(divB)
@@ -41,6 +43,15 @@ var drawConnector = function(divA, divB, edge) {
     edgeyA = yA + hA + 10;
     curvex = edgexA;
     curvey = edgeyB;
+  }
+
+  if(yA == yB){
+    edgexA = xA + wA;
+    edgeyA = yA + hA / 2;
+    edgexB = xB - 10;
+    edgeyB = yB + hB / 2;
+    curvex = edgexA + ((edgexB - edgexA) / 2);
+    curvey = edgeyA - 70;
   }
 
   $(edge).attr("d", "M"+edgexA+" "+edgeyA+" Q"+curvex+" "+curvey+" "+edgexB+" "+edgeyB);

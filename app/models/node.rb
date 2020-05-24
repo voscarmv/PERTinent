1047828d2019f1bcfb9e9e3097f9c11e648f05b1
Oscar Mier <voscarmv@gmail.com>
2020-05-24 15:07:15 -0500

    Made it impossible to delete root node.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,8 @@
 class Node < ApplicationRecord
+  before_destroy :check_if_root
+  class Error < StandardError
+  end
+
   has_many :pomodoro
   
   has_many :to_links, foreign_key: :from_id, class_name: :Link #tricky!
@@ -6,4 +10,11 @@ class Node < ApplicationRecord
 
   has_many :from_links, foreign_key: :to_id, class_name: :Link #tricky!
   has_many :from_nodes, through: :from_links
+
+  def check_if_root
+    root = Node.where(project_id: project_id).order(:created_at).limit(1).first.id
+    if id == root
+      raise Error.new "Can't delete root node"
+    end
+  end
 end

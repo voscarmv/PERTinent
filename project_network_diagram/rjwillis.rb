9f31347cd858fd6c2eb5c994423bd97ce116d8f2
Oscar Mier <voscarmv@gmail.com>
2020-06-20 06:08:25 -0500

    Worked on NPD algorithm.

--- project_network_diagram/rjwillis.rb
+++ project_network_diagram/rjwillis.rb
@@ -1,7 +1,7 @@
 # Algorithm from https://doi.org/10.1016/0305-0548(85)90041-3
 # An algorithm for constructing project network diagrams on an ordinary line printer
 # R.J.Willis†
-
+nodes = [1, 2, 3, 4, 5]
 pnd = [
   [[1, 2], 1],
   [[1, 3], 1],
@@ -12,7 +12,7 @@ pnd = [
   [[3, 5], 4]  
 ]
 
-table = Array.new(5){Array.new(4,0)}
+table = Array.new(nodes.length){Array.new(1,0)}
 
 pnd.each{ |activity|
   startnode = activity[0][0]
@@ -26,61 +26,34 @@ pnd.each{ |activity|
   else
     nest1e = 5
   end
-  
-  # x = table[nest1s].index{ |c| c == 0 }
 
   x = 0
   cols = table.transpose
-  puts "Transpose"
-  cols.each{ |r|
-    p r
-  }
-
-  puts "Table"
-  table.each{ |r|
-    p r
-  }  
-
-  puts "startnode = #{startnode}"
-  puts "endnode = #{endnode}"
-  puts "nest1s = #{nest1s}"
-  puts "nest1e = #{nest1e}"
-  while x < 4 do
-    puts "x = #{x}"
-    puts "cols[x]"
-    p cols[x]
-    # colx = col[x][nest1s-1...]
-    puts "then delete at cols[x][#{nest1s-1}] = #{cols[x][nest1s-1]}"
-    cols[x].delete_at(nest1s-1)
-    puts "after delete"
-    p cols[x]
-    puts "first delete at cols[x][#{nest1e-2}] = #{cols[x][nest1e-2]}"
-    cols[x].delete_at(nest1e-2)
-    puts "cols"
-    p cols[x]
-    cols[x].shift(nest1s-1)
+  found = false
+  while x < cols.length do
+    cols[x].delete_at(nest1e-1)
+    cols[x].shift(nest1s)
     if cols[x].all?(0)
       table[nest1s-1][x] = startnode
       table[nest1e-1][x] = endnode
+      found = true
       break
     end
-
-    # if nest1s + 1 == nest1e
-    #   table[nest1s-1][x] = startnode
-    #   table[nest1e-1][x] = endnode
-    #   break
-    # end
-    # if colx[nest1s...(nest1e-1)].all?{|v| v == 0}  
-    #   table[nest1s-1][x] = startnode
-    #   table[nest1e-1][x] = endnode
-    #   break
-    # end
-
-    table.each{ |r|
-      p r
-    }
-    
     x += 1
+  end
+  until found do
+    table.each{ |row|
+      row.push(0)
+    }
+    cols = table.transpose
+    cols[x].delete_at(nest1e-1)
+    cols[x].shift(nest1s)
+    if cols[x].all?(0)
+      table[nest1s-1][x] = startnode
+      table[nest1e-1][x] = endnode
+      found = true
+    end
+    x += 1    
   end
 }
 

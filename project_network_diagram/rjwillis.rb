480d1f589c56de92cc9854c585553b0f61dbadc4
Oscar Mier <voscarmv@gmail.com>
2020-06-24 01:21:09 -0500

    Debugged PND algo

--- project_network_diagram/rjwillis.rb
+++ project_network_diagram/rjwillis.rb
@@ -2,19 +2,34 @@
 # An algorithm for constructing project network diagrams on an ordinary line printer
 # R.J.Willis†
 nodes = [1, 2, 3, 4, 5]
+# pnd = [
+#   [[1, 2], 1],
+#   [[1, 3], 1],
+#   [[1, 4], 1],
+#   [[2, 3], 2],
+#   [[2, 5], 2],
+#   [[4, 5], 3],
+#   [[3, 5], 4]  
+# ]
+
 pnd = [
   [[1, 2], 1],
   [[1, 3], 1],
   [[1, 4], 1],
-  [[2, 3], 2],
   [[2, 5], 2],
-  [[4, 5], 3],
-  [[3, 5], 4]  
+  [[3, 5], 3],
+  [[4, 5], 4]
 ]
 
 table = Array.new(nodes.length){Array.new(1,0)}
 
 pnd.each{ |activity|
+
+  puts "TABLE START"
+  table.each{ |r|
+    p r
+  }
+
   startnode = activity[0][0]
   nest1s = activity[1]
 
@@ -24,19 +39,27 @@ pnd.each{ |activity|
   if ix
     nest1e = pnd[ix][1]
   else
-    nest1e = 5
+    nest1e = pnd.last[1]+1
   end
 
-  x = 0
+  puts "#{startnode},#{endnode}"
+  x = table[nest1s - 1].index {|c| c != 0}
+  unless x
+    x = 0
+  end
   cols = table.transpose
+
   found = false
   while x < cols.length do
+    puts "colx"
+    p cols[x]
     cols[x].delete_at(nest1e-1)
     cols[x].shift(nest1s)
     if cols[x].all?(0)
       table[nest1s-1][x] = startnode
       table[nest1e-1][x] = endnode
       found = true
+      puts "found1"
       break
     end
     x += 1
@@ -52,9 +75,16 @@ pnd.each{ |activity|
       table[nest1s-1][x] = startnode
       table[nest1e-1][x] = endnode
       found = true
+      puts "found2"
     end
     x += 1    
   end
+
+  puts "TABLE END"
+  table.each{ |r|
+    p r
+  }
+
 }
 
 puts "FINAL TABLE"
@@ -79,7 +109,7 @@ table.each{ |r|
   r.reverse.each_with_index{ |v, j|
     ix = r.length - 1 - j
     if v != 0
-      plot[i+3][ix] = " | "
+      plot[i+3][ix] = " v "
       plot[i+4][ix] = " v "
       if ix != e
         # puts "plot[i-1][ix] #{plot[i-1][ix]}"
@@ -92,14 +122,14 @@ table.each{ |r|
     end
     if ix != e
       if plot[i-1][ix] == " v "
-        plot[i][ix] = " | "
+        plot[i][ix] = " v "
         plot[i+1][ix] = " v "
-        plot[i+2][ix] = " | "
-        plot[i+3][ix] = " | "
+        plot[i+2][ix] = " v "
+        plot[i+3][ix] = " v "
         plot[i+4][ix] = " v "
       end
       if lf
-        plot[i][ix] = "<--"
+        plot[i][ix] = "<<<"
         plot[i+1][ix] = "   "
         plot[i+2][ix] = "   "
         plot[i+3][ix] = "   "
@@ -107,13 +137,13 @@ table.each{ |r|
       end
       if rf
         if plot[i-1][ix] == " v "
-          plot[i][ix] = " | "
+          plot[i][ix] = " v "
           plot[i+1][ix] = " v "
-          plot[i+2][ix] = "-C>"
-          plot[i+3][ix] = " | "
+          plot[i+2][ix] = ">v>"
+          plot[i+3][ix] = " v "
           plot[i+4][ix] = " v "
         else
-          plot[i+2][ix] = "-->"
+          plot[i+2][ix] = ">>>"
         end
       end
     end
